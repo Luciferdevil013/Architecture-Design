bcba014ee98d20e940a8cfa6d699adb7ca4b52b9
Luciferdevil013 <sujeetyadav8004100@gmail.com>
2025-04-25 16:35:20 +0530

    Fix the Card Size

--- src/Compoents/ServiceCard.jsx
+++ src/Compoents/ServiceCard.jsx
@@ -5,19 +5,21 @@ import { GoChevronUp } from "react-icons/go";
 
 function ServiceCard({index,expendable,setexpendable,heading,semiDetails,details}) {
     const [fullheight,setfullheight] = useState(0)
+    const [height,setheight] = useState(0)
     const divheight = useRef(null);
     useEffect(() => {
         if(expendable === index && divheight.current){
-
-            const h3 = divheight.current.querySelector('.h3-heading');
-            const h5 = divheight.current.querySelector('.h5-heading');
-            const para = divheight.current.querySelector('.paragraph');
-
-            const totalHeight = h3.getBoundingClientRect().height + h5.getBoundingClientRect().height + para.getBoundingClientRect().height + 11 * 3 + 15;
-
+            const totalHeight = divheight.current.scrollHeight;
             setfullheight(totalHeight);
         }
     },[expendable,index])
+
+    useEffect(() => {
+        if(divheight.current){
+            const h3 = divheight.current.querySelector('.h3-heading');
+            setheight(h3.scrollHeight + 20);
+        }
+    },[])
 
 
     const ChangeShow = () => {
@@ -28,7 +30,7 @@ function ServiceCard({index,expendable,setexpendable,heading,semiDetails,details
         <>
             <div onClick={ChangeShow} 
             ref={divheight} className='serviceCard' style={{
-                height: expendable === index ? `${fullheight}px` : '8vh',
+                height: expendable === index ? `${fullheight}px` : `${height}px`,
                 }}>
                 <h3 className='h3-heading'>{heading} <GoChevronUp /></h3>
                 <h5 className='h5-heading'>{semiDetails}</h5>
